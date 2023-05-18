aaa27e8ff44dd03fb915a4c3d67cf1cbc40b682c
Carlos-Guanipa <carlosguanipa@kabalaw.com>
2023-05-17 22:39:04 -0400

    Added new helper

--- app/components/HUIComboBox/HUIComboBoxInput.tsx
+++ app/components/HUIComboBox/HUIComboBoxInput.tsx
@@ -4,23 +4,18 @@ import { chakra } from "@chakra-ui/react";
 import { ComboboxInputProps, Combobox as HUIComboBox } from "@headlessui/react";
 import { forwardRef } from "react";
 
+import { suggestionToString } from "../../helpers/suggestionToString";
 import { ISuggestion } from "../../types";
 
 const { Input: HUIInput } = HUIComboBox;
-
-const cityToString = (suggestion: ISuggestion) => {
-  const cityString = suggestion.country.concat(
-    `, ${suggestion.name}${suggestion.state ? `, ${suggestion.state}` : ""}`,
-  );
-
-  return cityString;
-};
 
 const ForwardedHUIComboBoxInput = forwardRef<
   HTMLInputElement,
   ComboboxInputProps<"input", ISuggestion>
 >(({ ...props }, ref) => {
-  return <HUIInput ref={ref} displayValue={(city) => cityToString(city)} name={"q"} {...props} />;
+  return (
+    <HUIInput ref={ref} displayValue={(city) => suggestionToString(city)} name={"q"} {...props} />
+  );
 });
 
 ForwardedHUIComboBoxInput.displayName = "HUIComboBoxInput";
